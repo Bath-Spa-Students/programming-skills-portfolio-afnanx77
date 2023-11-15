2cc49e29570e30187b906fb6005a7ace58deefe7
afnanx77 <147949215+afnanx77@users.noreply.github.com>
2023-11-16 01:28:50 +0400

    Exercise1.py
    This reverts commit b4d141c98d7bef06c24559c873dcebdd59453bf6.

--- Chapter 4- Control Flow/Exercises/Exercise1.py
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -3,8 +3,10 @@
 •Write an if statement to test whether the aliens color is green. If it is, print a message that the player just earned 5 points.
 •Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)'''
 
-alien_color = "yellow"
-if alien_color == "yellow":
-    print("Congrats! You have earned 8 points!")
+alien_color = input("Enter color : ")
 if alien_color == "green":
-    print("Sorry! You lost 3 points!")
+    print("Congrats! You have earned 7 points!")
+elif alien_color == "red":
+    print("sorry! You have earned 0 points")
+else:
+    exit
